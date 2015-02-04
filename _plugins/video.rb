9c34673546fde8df7b33755846afc7aa20d99b98
Eric Paxton <heypaxton@gmail.com>
2015-02-04 15:05:07 -0800

    Updates 404 path

--- _plugins/video.rb
+++ _plugins/video.rb
@@ -5,21 +5,15 @@ class Video < Liquid::Tag
 
     if markup =~ Syntax then
       @id = $1
-
-      if $2.nil? then
-        @width = 560
-        @height = 420
-      else
-        @width = $2.to_i
-        @height = $2.to_i
-      end
+      @width = 560
+      @height = 420
     else
       raise "No YouTube ID provided in the tag"
     end
   end
 
   def render(context)
-    "<iframe frameborder=\"0\" width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+    "<iframe sevice=\"#{@service}\" frameborder=\"0\" width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
   end
 
    Liquid::Template.register_tag "video", self
