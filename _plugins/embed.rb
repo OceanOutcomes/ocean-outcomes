4f29a65a229e5d0567a426277f6860968e72bb51
Eric Paxton <heypaxton@gmail.com>
2016-04-04 15:31:46 -0700

    Update video plugin to allow map embeds

--- _plugins/embed.rb
+++ _plugins/embed.rb
@@ -1,4 +1,4 @@
-class Video < Liquid::Tag
+class Embed < Liquid::Tag
   Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
   def initialize(tagName, markup, tokens)
     super
@@ -8,7 +8,7 @@ class Video < Liquid::Tag
     markup.scan(Liquid::TagAttributes) do |key, value|
       @attributes[key] = value.gsub(/^'|"/, '').gsub(/'|"$/, '')
     end
-    
+
     if markup =~ Syntax then
       @id = @attributes['id']
       @service = @attributes['channel']
@@ -20,15 +20,17 @@ class Video < Liquid::Tag
   end
 
   def render(context)
-    case @service 
+    case @service
       when "youtube"
         "<iframe frameborder=\"0\" width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
-      when "facebook" 
+      when "facebook"
         "<iframe src=\"http://www.facebook.com/video/embed?video_id=#{@id}\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\"></iframe>"
       when "vimeo"
         "<iframe src=\"//player.vimeo.com/video/#{@id}?badge=0\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+      when "map"
+        "<iframe src=\"https://www.google.com\/maps\/embed?pb=#{@id} width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" style=\"border:0\" allowfullscreen><\/iframe>"
     end
   end
 
-   Liquid::Template.register_tag "video", self
+   Liquid::Template.register_tag "embed", self
 end
