ac3083e03da681ab702007bebda8560163a7d43a
Perry <pbennyb@users.noreply.github.com>
2018-03-08 10:53:38 -0800

    Create index.m

---
+++ what-we-do/testimonials/unfao/index.m
@@ -0,0 +1,7 @@
+---
+layout: testimonial
+title: The tuna harvest strategy workshops designed and managed by Ocean Outcomes helped us effectively engage a hundred and fifty key tuna fisheries managers and representatives in Indian, Atlantic, and Pacific Ocean coastal states as part of our Common Oceans ABNJ Tuna Project. The O2 team understands fisheries management and how to creatively communicate important principles to build improved understanding and decision making capacity.
+hero-image: https://s3-us-west-2.amazonaws.com/staticassets.oceanoutcomes.org/embedded+photos/testimonials/unfao-testimonial.png
+teaser-text: Alejandro Anganuzzi, Global Tuna Coordinator, Food and Agriculture Organization of the United Nations
+weight: 2
+---
